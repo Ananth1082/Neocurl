d74224f0ab477763a43865eebfcdde99f7cfcb37
Ananth1082 <ananthshetty203@gmail.com>
2024-08-26 22:48:47 +0530

    file watcher setup

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Ananth1082/m-v0.0
 
 go 1.23.0
+
+require github.com/fsnotify/fsnotify v1.7.0
+
+require golang.org/x/sys v0.4.0 // indirect
--- watchman/file_watcher.go
+++ watchman/file_watcher.go
@@ -1,5 +1,50 @@
 package watchman
 
-func Run() {
+import (
+	"log"
 
+	"github.com/fsnotify/fsnotify"
+)
+
+func Run() {
+	watcher()
+}
+
+func watcher() {
+	// Create new watcher.
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Start listening for events.
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Has(fsnotify.Write) {
+					log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	// Add a path.
+	err = watcher.Add("./tmp")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Block main goroutine forever.
+	<-make(chan struct{})
 }
