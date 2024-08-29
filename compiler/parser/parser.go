0b02a2cabdf59cbf8122eeef0347719c8c02d24e
Ananth Shetty <ananthshetty203@gmail.com>
2024-08-29 22:17:13 +0530

    Basic init

---
+++ compiler/parser/parser.go
@@ -0,0 +1,15 @@
+package parser
+
+import "os"
+
+func ReadFile(filePath string) (string, error) {
+	filebyt, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(filebyt), nil
+}
+
+func Separtor(program string) []string {
+
+}
--- todo.md
+++ todo.md
@@ -3,3 +3,7 @@
 1) Try to implement file watcher in go.
   - Implement a go program that watches file for changes and prints a message on change.
 2) Understand AST to implement route searcher. 
+
+### Aug 29/08/2024
+
+1) Create a tokenizer
