ede23d21f23493765b0f8bf9d7712d67ff04125f
Ananth Shetty <ananthshetty203@gmail.com>
2024-09-02 19:58:44 +0530

    added varaibles and verb parser

--- compiler/parser.go
+++ compiler/parser.go
@@ -1,1 +1,43 @@
 package compiler
+
+import (
+	"regexp"
+	"strings"
+)
+
+var minifyExp *regexp.Regexp 
+
+func init() {
+	minifyExp,err = regexp.Compile(` *\n(?:\s*)+`)
+	if err!=nil {
+		log.Fatal(err)
+	}
+}
+
+func ParseProgram(program string) {
+	cname, program := ParseCollectionName(program)
+	
+}
+
+func minifyProgram(program string) string {
+	
+}
+
+func ParseCollectionName(program string) (string, string) {
+	start, end := -1, -1
+	for i, letter := range program {
+		if letter == rune('#') {
+			start = i
+		}
+		if start != -1 && letter == rune('\n') {
+			end = i
+			break
+		}
+	}
+	return strings.TrimSpace(program[start:end]), program[end:]
+}
+
+func SetVaraiblles(program string) map[string]string {
+	start, end := -1, -1
+	for 
+}
--- main/main.go
+++ main/main.go
@@ -1,7 +1,133 @@
 package main
 
-import "github.com/Ananth1082/m-v0.0/watchman"
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var (
+	aliasValidator *regexp.Regexp = regexp.MustCompile(`^#([[:word:]]+)$`)
+	varValidator   *regexp.Regexp = regexp.MustCompile(`([[:word:]]+)="([[:word:]]+)"`)
+	verbValidator  *regexp.Regexp = regexp.MustCompile(`(?P<verb>GET|POST|PUT|DELETE) +"(?P<url>.+)"`)
+	// urlValidator   *regexp.Regexp = regexp.MustCompile(`(?:http|https):\/\/(?:.*\/)*(?:.*)*`)
+	// identifierValidator *regexp.Regexp = regexp.MustCompile(`[[:word:]]`)
+)
+
+var (
+	ErrInvalidCollectionName = errors.New("invalid collection name")
+	ErrInvalidVariableName   = errors.New("invalid variable name")
+	ErrInvalidVerbName       = errors.New("invalid verb name")
+)
+
+type Request struct {
+	verb string
+	url  string
+	head map[string]string
+	body []byte
+}
 
 func main() {
-	watchman.Run()
+	script := `#Collection_name
+SET key1="value1"
+key2="value2"
+#Request1
+GET "https;//google.com"
+HEAD "This is the head
+*** *** ***
+"
+BODY "This is the body
+*** *** ***
+#Request2
+GET "https;//google.com"
+HEAD "This is the head
+*** *** ***
+"
+BODY "This is the body
+*** *** ***
+"`
+	lines := strings.Split(script, "\n")
+	fmt.Println("lines ", lines)
+	fmt.Println(ParseCollectionName(lines[0]))
+
+	boundaries := FindEndOfBlocks(lines)
+	bos := FindSetBlock(lines[boundaries[0]:boundaries[1]])
+	fmt.Println(ParseVariables(lines[bos:boundaries[1]]))
+	prev := 0
+	for ind := range boundaries {
+		fmt.Println(lines[prev:ind])
+		prev = ind
+	}
+
+}
+
+// func processRequest() []Request {
+// 	requests := make([]Request, 0, n)
+
+// 	for
+
+// 	return requests
+// }
+
+func ParseCollectionName(collectionLine string) (string, error) {
+	cname := aliasValidator.FindStringSubmatch(collectionLine)
+	if cname == nil {
+		return "", ErrInvalidCollectionName
+	}
+	return cname[1], nil
+}
+
+func ParseVariables(setBlock []string) (map[string]string, error) {
+	varLookUpTable := make(map[string]string)
+	for _, line := range setBlock {
+		matches := varValidator.FindStringSubmatch(line)
+		if matches == nil {
+			return nil, ErrInvalidVariableName
+		}
+		key, value := matches[1], matches[2]
+		fmt.Println(key, ":", value)
+		varLookUpTable[key] = value
+	}
+	return varLookUpTable, nil
+}
+
+func ParseRequest() {
+
+}
+
+func FindEndOfBlocks(lines []string) []int {
+	var boundaries []int
+	for i, line := range lines {
+		if aliasValidator.MatchString(line) {
+			boundaries = append(boundaries, i)
+		}
+	}
+	return boundaries
+}
+
+func FindSetBlock(metaBlock []string) int {
+	for i, line := range metaBlock {
+		if strings.HasPrefix(line, "SET") {
+			return i
+		}
+	}
+	return -1
+}
+
+func (r *Request) setVerbAndURL(verbLine string) error {
+	matches := verbValidator.FindStringSubmatch(verbLine)
+	if matches == nil {
+		return ErrInvalidVerbName
+	}
+	r.verb, r.url = matches[1], matches[2]
+	return nil
+}
+
+func setHead() {
+
+}
+
+func setBody() {
+
 }
